f02bdea506ab2206005ea97b78815881a11c76d7
roberto <roberto.manzano96@gmail.com>
2021-10-06 18:22:02 -0400

    Delete redundant folder

--- sprint-three/server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const videos = require('./videos');
-const internal = require('./Internal');
-
-router.use('/api/videos', videos);
-router.use('/api/internal', internal);
-
-module.exports = router;
